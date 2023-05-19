80da81363e06d89e5be48690ba83e52690d79a72
Alexandra Dernova <alexder@gmx.net>
2023-05-19 10:14:27 -0700

    card erstellt

--- components/card/card.js
+++ components/card/card.js
@@ -1,25 +1,25 @@
-export function createCharacterCard() {
+export function createCharacterCard(character) {
     const card = document.createElement("li");
     card.classList.add("card");
     card.innerHTML = `
     <div class="card__image-container">
     <img
       class="card__image"
-      src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+      src=${character.image}
       alt="Rick Sanchez"
     />
     <div class="card__image-gradient"></div>
   </div>
   <div class="card__content">
-    <h2 class="card__title">Rick Sanchez</h2>
+    <h2 class="card__title">${character.name}</h2>
     <dl class="card__info">
       <dt class="card__info-title">Status</dt>
-      <dd class="card__info-description">Alive</dd>
+      <dd class="card__info-description">${character.status}</dd>
       <dt class="card__info-title">Type</dt>
-      <dd class="card__info-description"></dd>
+      <dd class="card__info-description">${character.title}</dd>
       <dt class="card__info-title">Occurrences</dt>
-      <dd class="card__info-description">51</dd>
+      <dd class="card__info-description">${character.episode}</dd>
     </dl>
-  </div>
+  </div>`;
 return card;
 }
